6a36098461598c726af2e1f5185475f2ed6d452a
SPADGELET-PC\Ian <imjberry@gmail.com>
2020-06-21 09:58:58 +0200

    Class name comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,8 @@ class ArticlesController < ApplicationController
     # Initialize model with desired attributes
     # These are mapped to their respective db columns
     @article = Article.new(params[:article])
- 
+    # A for Article is class name. new instantiates a new instance
+    
     @article.save  # Save in db
     redirect_to @article  # Redirect to show action
   end
