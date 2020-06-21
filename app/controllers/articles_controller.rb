4a53ee218e81a95f1d26c7b5b3d571a17bebf19e
SPADGELET-PC\Ian <imjberry@gmail.com>
2020-06-21 10:08:36 +0200

    Require and permit params for db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,16 @@ class ArticlesController < ApplicationController
     
     # Initialize model with desired attributes
     # These are mapped to their respective db columns
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)  # params defined below
     # A for Article is class name. new instantiates a new instance
-    
+
     @article.save  # Save in db
     redirect_to @article  # Redirect to show action
+    
+    # Set the require and permit params
+    private
+      def article_params
+        params.require(:article).permit(:title, :text)
+      end
   end
 end
