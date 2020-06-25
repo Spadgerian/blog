9e5f5bfe9406f8ba7306919bdb76c371690c5550
SPADGELET-PC\Ian <imjberry@gmail.com>
2020-06-25 18:20:14 +0200

    5.11 Add update method to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   # Set the require and permit params
   private
     def article_params
